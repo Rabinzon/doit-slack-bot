9b728c1bd54dde720c5c76b3facca05784bf5561
rabinzon <ildarrazin@gmail.com>
2016-11-17 09:36:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const Bot = require('slackbots');
+const config = require('./.config.json');
+const triggers = require('./triggers.json');
+
+const bot = new Bot(config);
+
+const params = {
+	icon_url: 'http://i.imgur.com/xQNBz0U.png'
+};
+
+const check = (message) =>
+	message.subtype !== 'bot_message'
+		&& message.type === 'message';
+
+const getTrigger = (message) =>
+	triggers.filter(t => new RegExp(t.w, 'gi').test(message.text))[0];
+
+const getAnswer = (answers) => {
+	const index = Math.floor(Math.random() * (answers.length - 0));
+	return answers[index];
+};
+
+bot.on('message', (message) => {
+	if (check(message)) {
+		const result = getAnswer(getTrigger(message).a);
+		bot.postMessageToChannel('general', result, params, console.log);
+	}
+});
